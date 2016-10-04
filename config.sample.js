cdb05cec57b4beb679c924b2da5669dcb9f6bd40
sapphire <info@sapphire.sh>
2016-10-05 02:20:11 +0900

    fix travis-ci config

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,6 @@
 module.exports = {
-	consumer_key: '',
-	consumer_secret: '',
+	consumer_key: 'IQKbtAYlXLripLGPWd0HUA',
+	consumer_secret: 'GgDYlkSvaPxGxC4X8liwpUoqKwwr3lCADbz8A7ADU',
 	access_token: '',
 	access_token_secret: ''
 };
